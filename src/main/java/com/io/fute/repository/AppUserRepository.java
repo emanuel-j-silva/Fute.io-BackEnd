fede5206071db16e50ec95c0a21484b566e7ac5a
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-04-04 11:15:32 -0300

    fix: add annotation to app user repository

--- src/main/java/com/io/fute/repository/AppUserRepository.java
+++ src/main/java/com/io/fute/repository/AppUserRepository.java
@@ -3,9 +3,11 @@ package com.io.fute.repository;
 import com.io.fute.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
     UserDetails findByUsername(String username);
 }
