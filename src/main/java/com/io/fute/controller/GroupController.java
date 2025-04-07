6c283ef3a2f7307c40cdf893d33d30519c6d10bf
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-04-07 10:59:36 -0300

    feat: create group endpoints

---
+++ src/main/java/com/io/fute/controller/GroupController.java
@@ -0,0 +1,50 @@
+package com.io.fute.controller;
+
+import com.io.fute.dto.GroupInfo;
+import com.io.fute.dto.GroupRequest;
+import com.io.fute.entity.AppUser;
+import com.io.fute.entity.Group;
+import com.io.fute.service.GroupService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/groups")
+public class GroupController {
+    private final GroupService groupService;
+
+    @Autowired
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createGroup(@RequestBody @Valid GroupRequest request){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        AppUser user = (AppUser) authentication.getPrincipal();
+
+        groupService.createGroup(request, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Grupo criado com sucesso");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GroupInfo>> fetchGroupsByUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        AppUser user = (AppUser) authentication.getPrincipal();
+
+        List<Group> groups = groupService.fetchAllGroupsByUser(user.getId());
+
+        List<GroupInfo> groupInfos = groups.stream()
+                .map(group -> new GroupInfo(group.getName(),group.getLocation(), group.getNumberOfPlayers()))
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(groupInfos);
+    }
+}
