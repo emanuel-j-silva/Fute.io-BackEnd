7bf0b6cf3744047b0cff795701f363099185bf91
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-04-09 12:39:23 -0300

    chore: remove unnecessary imports

--- src/main/java/com/io/fute/dto/group/GroupRequest.java
+++ src/main/java/com/io/fute/dto/group/GroupRequest.java
@@ -1,9 +1,6 @@
 package com.io.fute.dto.group;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-import java.util.UUID;
 
 public record GroupRequest(@NotBlank String name, @NotBlank String location) {
 }
