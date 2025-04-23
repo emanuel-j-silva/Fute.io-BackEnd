f2c4e30431f303aee5e55fde5890afcf50b8936f
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-04-23 20:35:45 -0300

    feat: create draw entity

---
+++ src/main/java/com/io/fute/entity/Draw.java
@@ -0,0 +1,32 @@
+package com.io.fute.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table
+public class Draw {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime date;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    @OneToMany(mappedBy = "draw_id", orphanRemoval = true)
+    private Set<Team> teams;
+
+    public Draw(){
+        this.date = LocalDateTime.now();
+    }
+
+    public Set<Team> fetchTeams(){
+        return teams;
+    }
+
+}
