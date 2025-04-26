c57c26f308ce3a0b44c10d7d145b4c1ef8983525
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-04-26 17:56:28 -0300

    test: add tests to null check in players list of perform draw method

--- src/test/java/com/io/fute/DrawTest.java
+++ src/test/java/com/io/fute/DrawTest.java
@@ -1,10 +1,14 @@
 package com.io.fute;
 
 import com.io.fute.entity.Draw;
+import com.io.fute.entity.Player;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 public class DrawTest {
@@ -26,6 +30,25 @@ public class DrawTest {
     @DisplayName("Should throw exception when fetch teams is called with teams set empty")
     void shouldThrowWhenTeamsSetIsEmpty(){
         assertThatThrownBy(draw::fetchTeams).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("Should throw exception when players list is null")
+    void shouldThrowPlayersListNull(){
+        assertThatThrownBy(()-> draw.perform(null,2))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    @DisplayName("Should throw exception when any player in the players list is null")
+    void shouldThrowPlayerIsNull(){
+        List<Player> players = new ArrayList<>();
+        players.add(null);
+        players.add(new Player());
+        assertThatThrownBy(()-> draw.perform(players,2))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 
 }
