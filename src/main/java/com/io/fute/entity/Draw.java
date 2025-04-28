65f035a1aeb06775cfb66ffe624f3cf77efe2ee5
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-04-28 15:02:06 -0300

    refactor: refactoring perform draw logic to pass with a mathematical criterion

--- src/main/java/com/io/fute/entity/Draw.java
+++ src/main/java/com/io/fute/entity/Draw.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.OptionalDouble;
 
 @Entity
 @Table
@@ -46,9 +48,23 @@ public class Draw {
         for(int i=0; i < numberOfTeams; i++){
             teams.add(new Team(String.valueOf(i+1)));
         }
-        for(int i=0; i < players.size(); i++){
-            int indexTeam = i % numberOfTeams;
-            teams.get(indexTeam).addPlayer(players.get(i));
+
+        boolean hasAcceptableDifference = false;
+        while(!hasAcceptableDifference){
+            for(Team t: teams){
+                t.removeAllPlayers();
+            }
+            Collections.shuffle(players);
+
+            for(int i=0; i < players.size(); i++){
+                int indexTeam = i % numberOfTeams;
+                teams.get(indexTeam).addPlayer(players.get(i));
+            }
+            OptionalDouble max = teams.stream().mapToDouble(Team::averageOverall).max();
+            OptionalDouble min = teams.stream().mapToDouble(Team::averageOverall).min();
+            if (max.isPresent() && min.isPresent()){
+                hasAcceptableDifference = (max.getAsDouble() - min.getAsDouble()) <= 15;
+            }
         }
 
     }
