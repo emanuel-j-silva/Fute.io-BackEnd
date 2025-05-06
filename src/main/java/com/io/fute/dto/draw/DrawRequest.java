479fd62b780666bbebf37f4f549b80f12d8b6d6b
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-05-06 00:04:08 -0300

    fix: use not null and min size instead of not blank annotation in draw request

--- src/main/java/com/io/fute/dto/draw/DrawRequest.java
+++ src/main/java/com/io/fute/dto/draw/DrawRequest.java
@@ -1,8 +1,9 @@
 package com.io.fute.dto.draw;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
-public record DrawRequest(@NotBlank List<Long> playerIds, int numberOfTeams) {
+public record DrawRequest(@NotNull @Size(min=3) List<Long> playerIds, int numberOfTeams) {
 }
